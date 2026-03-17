5c2c7fc85be83e6264ad1a712507aa9f47f54c76
Mikołaj Wolski <148907624+yungwolsky@users.noreply.github.com>
2026-03-17 20:56:03 +0100

    Edited influxUrl in Program.cs

--- backend/WeatherAPI/Program.cs
+++ backend/WeatherAPI/Program.cs
@@ -8,7 +8,7 @@ builder.WebHost.UseUrls("http://0.0.0.0:5273");
 
 builder.Services.AddSingleton<IInfluxDBClient>(sp =>
 {
-    var influxUrl = "http://localhost:8086";  
+    var influxUrl = "http://influxdb:8086";
     var token = "WeatherMonitor12345";         
     return InfluxDBClientFactory.Create(influxUrl, token.ToCharArray());
 });
